847adc11d307cc19c0c8bedaf317c05589cf155e
Ran <runrunbear@gmail.com>
2018-02-06 07:52:50 -0800

    Add files via upload

---
+++ 44、手把手教React Native实战之API学习-网络状态与数据交互【源码】.js
@@ -0,0 +1,269 @@
+﻿/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  NetInfo,
+  ToastAndroid,
+  View
+} from 'react-native';
+
+class DfyProject02 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isConnected: null,
+      connectionInfo: null,
+    };
+
+  }
+
+  componentDidMount() {
+    //网络是否连接的监听
+    NetInfo.isConnected.addEventListener(
+      'isConnected',
+      this._handleConnectivityChange
+    );
+
+    //网络状态变化的监听
+    NetInfo.addEventListener(
+      'statusChange',
+      this._handleNetStatusChange
+    );
+
+
+    //检测网络是否连接
+    NetInfo.isConnected.fetch().done(
+      (isConnected) => { this.setState({ isConnected: isConnected }); }
+    );
+    //检测网络连接状态
+    NetInfo.fetch().done(
+      (connectionInfo) => { this.setState({ connectionInfo }); }
+    );
+  }
+
+
+
+  componentWillUnmount() {
+    //卸载两个监听
+    NetInfo.isConnected.removeEventListener(
+      'isConnected',
+      this._handleConnectivityChange
+    );
+    NetInfo.removeEventListener(
+      'statusChange',
+      this._handleNetStatusChange
+    );
+  }
+
+
+  _handleConnectivityChange = (isConnected) => {
+    ToastAndroid.show((isConnected ? 'online' : 'offline'), ToastAndroid.SHORT);
+  }
+
+  _handleNetStatusChange = (status) => {
+
+    ToastAndroid.show('当然网络状态：' + status, ToastAndroid.SHORT);
+
+  }
+
+
+  render() {
+    return (
+      <View >
+        <Text style={styles.welcome}>
+          当前的网络状态
+        </Text>
+        <Text style={styles.welcome}>
+          {this.state.isConnected ? '网络在线' : '离线'}
+        </Text>
+        <Text style={styles.welcome}>
+          当前网络连接类型
+        </Text>
+        <Text style={styles.welcome}>
+          {this.state.connectionInfo}
+        </Text>
+        <Text style={styles.welcome}>
+          当前网络是否计费(仅Android平台)
+        </Text>
+        <Text style={styles.welcome}>
+          {NetInfo.isConnectionExpensive === true ? '需要计费' : '不要计费'}
+        </Text>
+
+        <Text onPress={this.goPostNet2.bind(this) } style={styles.btn}>fetch-POST访问网络1/2</Text>
+        <Text onPress={this.goGetNet.bind(this) } style={[styles.btn,{backgroundColor: 'blue'}]}>fetch-GET访问网络</Text>
+      </View>
+    );
+  }
+
+  goPostNet1() {
+    ToastAndroid.show('fetch-POST访问网络-方法1', ToastAndroid.SHORT);
+    let url = 'http://www.5iqunzi.com/wenjiaosuo/admin/privilege.php';
+    let map = { method: 'POST' };
+
+    let privateHeaders = {
+      'Private-header1': 'value1',
+      'Private-header2': 'value2',
+      'Content-Type': 'text/plain',
+      'User-Agent': 'testAgent',
+    };
+
+    // map.headers = privateHeaders;
+    //post的数据 post才需要body
+    map.body = JSON.stringify(
+      {
+        'username': '13667377378',
+        'password': 'dfy889',
+        'act': 'signin',
+      }
+    );
+
+
+    //如果你的服务器无法识别上面POST的数据格式，那么可以尝试传统的form格式
+    //  map.body = 'username=13667377378&password=dfy889&act=signin';
+    map.follow = 10;//设置请求允许的最大重定向次数，0为不允许重定向
+    map.timeout = 8000;//设置超时时间，0为没有超时时间，这个值在重定向时会被重置
+    map.size = 0;//设置请求回应中的消息体最大允许长度，0为没有限制
+
+    console.log('fetch-POST访问网络-方法1');
+
+    fetch(url, map).then(
+      (response) => {
+        console.log('fetch-第一个then里面');
+        return response.text();
+
+      }
+      // (response) => response.text()
+    ).then(
+      (responseText) => {
+        //这里不能用console否则看不到 这是一个巨大的坑 用alert一点问题没有
+        alert('服务器返回：' + responseText);
+        // console.log('服务器返回：' + responseText);
+      }
+      ).catch(
+      (err) => {
+        console.log('错误：' + err);
+      }
+      );
+
+  }
+
+
+  postRequest(url, data, callback) {
+    let map = {
+      method: 'POST',
+      headers: {
+        'Private-header1': 'value1',
+        'Private-header2': 'value2',
+        'Content-Type': 'text/plain',
+        'User-Agent': 'testAgent',
+      },
+      body: JSON.stringify(data),
+      follow: 20,
+      timeout: 8000,
+      size: 0,
+    };
+
+    fetch(url, map).then((response) => response.text()).then(
+      (responseText) => {
+        callback(responseText);
+      }
+    ).catch(
+      (err) => {
+        callback(err);
+      }
+      );
+
+  }
+
+  goPostNet2() {
+    ToastAndroid.show('fetch-POST访问网络-方法2', ToastAndroid.SHORT);
+    let url = 'http://www.5iqunzi.com/wenjiaosuo/admin/privilege.php';
+    let data = {
+      'username': '13667377378',
+      'password': 'dfy889',
+      'act': 'signin',
+    };
+    console.log('fetch-POST访问网络-方法2');
+    this.postRequest(url, data, (result) => {
+      alert('服务器返回结果：' + result);
+      // console.log('服务器返回结果：' + result);
+    });
+
+  }
+
+  goGetNet() {
+    ToastAndroid.show('fetch-GET访问网络', ToastAndroid.SHORT);
+    console.log('GET访问网络');
+    let url = 'http://www.reactnative.vip/';
+    fetch(url).then(
+      (response) => {
+        console.log('第一个then里面');
+        return response.text()
+      }
+    ).then(
+      (responseText) => {
+        //这里不能用console否则看不到 这是一个巨大的坑 用alert一点问题没有
+        alert('服务器返回：' + responseText);
+        // console.log('服务器返回：' + responseText);
+      }
+      ).catch(
+      (err) => {
+        console.log('err' + err);
+      }
+      );
+  }
+
+
+
+}
+
+
+
+const styles = StyleSheet.create({
+
+
+
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  btn: {
+
+    backgroundColor: '#FF7200',
+    height: 33,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: '#fff',
+    marginLeft: 10,
+    marginRight: 10,
+    lineHeight: 24,
+    marginTop: 40,
+    fontSize: 18,
+
+
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+
+AppRegistry.registerComponent('DfyProject02', () => DfyProject02);
+
---
+++ 47、TimerDemo.js
@@ -0,0 +1,152 @@
+﻿/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import moment from 'moment';
+import Geolocation from 'Geolocation';
+
+let lastTime=0;
+let currentTime=0;
+
+class TimerDemo extends Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 0
+    };
+  }
+
+  render() {
+    console.log('render');
+    let css = [];
+    css.push(styles.progress);
+    if (this.state.width) {
+      css.push({ width: this.state.width });
+    }
+
+    return (
+      <View>
+        <Text onPress={this._setInterval} style={styles.btn}>setInterval</Text>
+        <View style={css}></View>
+      </View>
+    );
+  }
+
+
+  _setInterval() {
+    console.log('获取网页数据-当前时间为：'+moment().format('YYYY-MM-DD HH:mm:ss'));
+
+    this.interval = setInterval(() => {
+      fetch('http://www.reactnative.vip/')
+        .then(function (data) {
+          return data.text();
+        })
+        .then((responseText) => {
+          console.log('返回了数据，时间为：'+moment().format('YYYY-MM-DD HH:mm:ss'));
+          console.log(responseText);
+
+        })
+        .catch((error) => {
+          console.warn(error);
+        });
+    }, 5000);
+  }
+
+
+
+
+  componentDidMount() {
+    var _that = this;
+    //5秒之后取当前手机的位置
+    alert('开始要取位置：'+moment().format('YYYY-MM-DD HH:mm:ss'));
+    this.timer = setTimeout(() => {
+      Geolocation.getCurrentPosition((data) => {
+        //为了看到效果 不要用console输出，同时记得计时器一定要全部清空，否则会有问题，而且一时看不出
+        alert('5秒钟之后得到位置信息-'+JSON.stringify(data)+'当前时间为：'+moment().format('YYYY-MM-DD HH:mm:ss'));
+      }, (e) => {
+        alert(JSON.stringify(e));
+      });
+    }, 5000);
+
+
+    this.immediate = setImmediate(
+      () => {
+        console.log('立刻执行,当前时间-'+moment().format('YYYY-MM-DD HH:mm:ss'));
+      }
+    );
+
+
+    function doAnimated() {
+      _that.setState({
+        width: _that.state.width + 10
+      });
+      currentTime=new Date().getTime();
+      
+      console.log('当前的宽度：'+_that.state.width+'当前时间：'+currentTime+'---时间间隔：'+(currentTime-lastTime));
+      console.log('当前的宽度：'+_that.state.width+'当前时间：'+moment().format('YYYY-MM-DD HH:mm:ss'));
+      lastTime=currentTime;
+      if (_that.state.width < 300) {
+        requestAnimationFrame(doAnimated);
+      }
+    }
+
+
+    requestAnimationFrame(doAnimated);
+    
+
+
+    
+
+  }
+
+
+  componentWillUnmount() {
+    // 如果存在this.timer，则使用clearTimeout清空。
+    // 如果你使用多个timer，那么用多个变量，或者用个数组来保存引用，然后逐个clear
+    this.timer && clearTimeout(this.timer);
+
+    this.immediate && clearImmediate(this.immediate);
+
+    this.interval && clearInterval(this.interval);
+
+    console.log('清空所有的计时器');
+  }
+
+
+}
+
+const styles = StyleSheet.create({
+  btn: {
+    marginTop: 50,
+    marginLeft: 10,
+    marginRight: 10,
+    height: 35,
+    backgroundColor: '#3BC1FF',
+    color: '#fff',
+    lineHeight: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textAlignVertical:'center'
+  },
+  progress: {
+    height: 10,
+    width: 10,
+    marginLeft: 10,
+    backgroundColor: '#E72D00',
+    marginTop: 10
+  },
+});
+
+AppRegistry.registerComponent('TimerDemo', () => TimerDemo);
